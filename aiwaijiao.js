e4b79fe36bdf36abaecab0470bb791bfa2849a0b
dreamdeng <1297057965@qq.com>
2024-11-08 01:41:11 +0800

    Update aiwaijiao.js

--- aiwaijiao.js
+++ aiwaijiao.js
@@ -3,7 +3,7 @@
 ai外教
 
 [rewrite_local]
-^https:\/\/(chatai\.qupeiyin\.com\/member\/index|www\.longstargpt\.com\/member\/index\/chatgpt\/chat\/sseV2|chatai\.qupeiyin\.com\/lesson\/getLessons) url script-response-body https://raw.githubusercontent.com/Konghen001/myself/main/AIEnglish.js
+^https:\/\/(chatai\.qupeiyin\.com\/member\/index|www\.longstargpt\.com\/member\/index\/chatgpt\/chat\/sseV2|chatai\.qupeiyin\.com\/lesson\/getLessons) url script-response-body https://github.com/dreamdeng/script/blob/main/aiwaijiao.js
 
 [mitm] 
 

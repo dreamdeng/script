85ee88a8a44f47977ea77365fa1e49bfe9f10419
dreamdeng <CheneyRone@outlook.com>
2025-10-21 13:55:36 +0800

    Update fanyi.js

--- fanyi.js
+++ fanyi.js
@@ -1,1 +1,31 @@
+/**
+ * Quantumult X 重写脚本
+ * 用于修改 transyncai.com 用户时长统计接口返回
+ * 脚本地址: https://raw.githubusercontent.com/dreamdeng/script/refs/heads/main/fanyi.js
+ */
 
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 修改为VIP无限制数据
+if (obj.data) {
+    obj.data.active = true;
+    obj.data.premiumUser = true;
+    obj.data.subscriptionUser = true;
+    obj.data.subscriptionSource = "alipay";
+    obj.data.availableTime = 999999999;
+    obj.data.currentMonthTotalTime = 999999999;
+    obj.data.monthlyAvailableTime = 999999999;
+    obj.data.monthlyTotalTime = 999999999;
+    obj.data.pointAvailableTime = 0;
+    obj.data.pointTotalTime = 0;
+    obj.data.currentMonthUsedTime = 0;
+    obj.data.totalUsedTime = 0;
+    obj.data.currentPackageStartTime = 1729484581157;
+    obj.data.currentPackageEndTime = 4096244989000; // 2099年
+    obj.data.finalPackageEndTime = 1792470181157;
+    obj.data.remainingCreditMonth = 9999;
+}
+
+body = JSON.stringify(obj);
+$done({ body });
